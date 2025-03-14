ea8ceea94c5d87dee846f3954a742c24b8653f8c
taljindergill78 <taljindergill78@gmail.com>
2025-03-14 13:35:17 -0700

    Update 1407 Top Travellers.sql

--- 1407 Top Travellers.sql
+++ 1407 Top Travellers.sql
@@ -1,5 +1,5 @@
 -- Write your MySQL query statement below
-select a.name, ifnull(sum(b.distance),0) as travelled_distance
+select a.name, coalesce(sum(b.distance),0) as travelled_distance
 from Users a
 left join Rides b
 on a.id = b.user_id
